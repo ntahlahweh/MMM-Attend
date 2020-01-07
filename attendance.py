908192b479d3c3a71d973114f7c0b82390555edb
Hafiz Zaini <muhdhafizzaini@gmail.com>
2020-01-07 13:09:49 +0800

    get the attendance from ZKLib
    get the attendance from ZKLib

--- attendance.py
+++ attendance.py
@@ -13,17 +13,55 @@ file_exist = os.path.isfile(file_name)
 conn = None
 zk = ZK('192.168.1.201', port=4370, timeout=5, password=0, force_udp=False, ommit_ping=False) #connect with the zk terminal
 cur_data = 0
-wait_time = 1
+wait_time = 10
 
 class ProgramKilled(Exception):
     pass
 
 def foo():
     def staff_name(staff_id):
-        if staff_id == 0001:
-            Sname = "Test1"
-        elif staff_id == 0002:
-            Sname = "Test2" 
+        if staff_id == 1017:
+            Sname = "Muhammad Faris"
+        elif staff_id == 1014:
+            Sname = "Hani Izzati"
+        elif staff_id == 1009:
+            Sname = "Norwizana"
+        elif staff_id == 2:
+            Sname = "ADMIN"
+        elif staff_id == 1007:
+            Sname = "Siti Norfatiha"
+        elif staff_id == 1040:
+            Sname = "Nur Izzati"
+        elif staff_id == 1037:
+            Sname = "Muhammad Hafiz"
+        elif staff_id == 1001:
+            Sname = "Dr. Abdul Rahman"
+        elif staff_id == 1029:
+            Sname = "Mohd Hazizi"
+        elif staff_id == 1011:
+            Sname = "Adi Effendi"
+        elif staff_id == 1033:
+            Sname = "Muhammad Asyraf"
+        elif staff_id == 1006:
+            Sname = "Muhammad Nur Azizi"
+        elif staff_id == 1005:
+            Sname = "Nurul Saadah"
+        elif staff_id == 1020:
+            Sname = "Amirul Aiman"
+        elif staff_id == 1030:
+            Sname = "Khoshim"
+        elif staff_id == 1002:
+            Sname = "Sharifah Aishah"
+        elif staff_id == 1027:
+            Sname = "Khairinah Izzah"
+        elif staff_id == 1016:
+            Sname = "Azmi"
+        elif staff_id == 1021:
+            Sname = "Asyira"
+        elif staff_id == 1039:
+            Sname = "Aimi Khalidah"
+        elif staff_id == 1034:
+            Sname = "Mohammad Zulkhairi"
         else:
             Sname = "New staff, please register"
         return Sname
@@ -50,26 +88,20 @@ def foo():
                     attend_data = str(attend[data-1])
                     test, staff_id, test3, date_come, time_come, test6, test7=  attend_data.split() #take staff_id, date and time
                     #print('New attendance: ', staff_id, 'timestamp: ', date_come, time_come)
-                    print("Welcome! \n" + staff_name(int(staff_id)))
+                    #print("Welcome! \n" + staff_name(int(staff_id)))
                     nameStaff = staff_name(int(staff_id))
-                    conJson = {"Staff ID" : staff_id,
-                    "Staff Name" : nameStaff,
-                    "Time" : time_come}
+                    conJson = nameStaff
                     sendJson = json.dumps(conJson)
-                    #print(sendJson)
+                    print(sendJson)
+                    sys.stdout.flush()
                 else:
                     #print('No new entries')
                     stat = "No new Entries"
-                    attend_data = str(attend[data-1]) #nanti delete balik
-                    test, staff_id, test3, date_come, time_come, test6, test7=  attend_data.split() #take staff_id, date and time
-                    #print('New attendance: ', staff_id, 'timestamp: ', date_come, time_come)
-                    print ("No new staff")
-                    
             except:
                 print ('An excepetion occured')
                 
         else:
-            #print('File not exist')  #if file not exist create a new one and save the latest attendance in new_attend.txt
+            #if file not exist create a new one and save the latest attendance in new_attend.txt
             with open(file_name,'w', encoding = 'utf-8') as f:
                 f.write(str(data).decode('utf-8'))
                 f.close
@@ -77,13 +109,12 @@ def foo():
             attend_data = str(attend[data-1])
             test, staff_id, test3, date_come, time_come, test6, test7=  attend_data.split() #take staff_id, date and time
             #print('New attendance: ', staff_id, 'timestamp: ', date_come, time_come)
-            print(staff_name(int(staff_id)))
+            #print(staff_name(int(staff_id)))
             nameStaff = staff_name(int(staff_id))
-            conJson = {"Staff ID" : staff_id,
-            "Staff Name" : nameStaff,
-            "Time" : time_come}
+            conJson = nameStaff
             sendJson = json.dumps(conJson)
-            #print(sendJson)
+            print(sendJson)
+            sys.stdout.flush()
             
     except Exception as e:
         print ('Process terminate : {}'.format(e))
@@ -122,7 +153,7 @@ if __name__ == "__main__":
           try:
               time.sleep(1)
           except ProgramKilled:
-              print ("Program killed: running cleanup code")
+              #print ("Program killed: running cleanup code")
               job.stop()
               break        
     
